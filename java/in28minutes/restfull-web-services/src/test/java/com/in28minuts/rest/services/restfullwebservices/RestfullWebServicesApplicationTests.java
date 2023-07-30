f1c72438a09283b172fc59ba0948b7477c5f092a
SkCodeRains <68813335+SkCodeRains@users.noreply.github.com>
2023-07-30 15:35:41 +0530

    rest
    rest

---
+++ java/in28minutes/restfull-web-services/src/test/java/com/in28minuts/rest/services/restfullwebservices/RestfullWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minuts.rest.services.restfullwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
